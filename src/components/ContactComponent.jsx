27ff267fc8d5370af8b4ad839c1cf219b74d3037
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-02-23 22:28:31 -0800

    create contact component

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -1,1 +1,25 @@
-export default function ContactComponent() {}
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsEnvelope } from "react-icons/bs";
+
+export default function ContactComponent() {
+    return (
+        <ul className="contact-links">
+            <li className="contact-icons">
+                <a href="https://www.linkedin.com/in/annachuapetcharasopon/" target="_blank" rel="noreferrer">
+                    <BsLinkedin />
+                </a>
+            </li>
+            <li className="contact-icons">
+                <a href="https://github.com/ahgeak/" target="_blank" rel="noreferrer">
+                    <BsGithub />
+                </a>
+            </li>
+            <li className="contact-icons">
+                <a href="mailto:annaedwardson@gmail.com" target="_blank" rel="noreferrer">
+                    <BsEnvelope />
+                </a>
+            </li>
+        </ul>
+    );
+}
