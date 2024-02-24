f17f4af5833f36945d233228f84248d5496fbeea
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-02-23 22:04:34 -0800

    create navigation component

--- src/components/NavigationComponent.jsx
+++ src/components/NavigationComponent.jsx
@@ -1,1 +1,33 @@
-export default function NavigationComponent() {}
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function NavigationComponent() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand>Anna Chuapetcharasopon</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Item>
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
