254a69dc79f3a4ec97ff68dd35bbb6e2729bcfd9
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-02-23 22:35:44 -0800

    create footer component

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,1 +1,37 @@
-export default function FooterComponent() {}
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function FooterComponent() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Developed by Anna Chuapetcharasopon</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} </h3>
+        </Col>
+        <Col md="4" className="footer-body">
+            <ul className="contact-links">
+                <li className="contact-icons">
+                    <a href="https://www.linkedin.com/in/annachuapetcharasopon/" target="_blank" rel="noreferrer">
+                        <BsLinkedin />
+                    </a>
+                </li>
+                <li className="contact-icons">
+                    <a href="https://github.com/ahgeak/" target="_blank" rel="noreferrer">
+                        <BsGithub />
+                    </a>
+                </li>
+                <li className="contact-icons">
+                    <a href="mailto:annaedwardson@gmail.com" target="_blank" rel="noreferrer">
+                        <BsEnvelope />
+                    </a>
+                </li>
+            </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
