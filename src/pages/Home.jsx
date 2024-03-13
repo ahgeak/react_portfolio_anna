6c3dcbfff8ae53737872b813ecf25d464b9d92aa
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-03-13 00:24:52 -0700

    Home.js: add image for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,17 @@ import HeaderComponent from '../components/HeaderComponent';
 // import NavigationComponent from '../components/NavigationComponent';
 import FooterComponent from '../components/FooterComponent';
 
+import { Card } from "react-bootstrap";
+
 const Home = () => {
   return (
     <React.Fragment>
-        {/* <div style={{ backgroundImage: `url(${background})` }}></div> */}
         <HeaderComponent />
+        <Card>
+            {/* <Card.Img src="https://images.unsplash.com/photo-1537498425277-c283d32ef9db?q=80&w=1756&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />  */}
+            {/* <Card.Img src="https://images.unsplash.com/photo-1606327054536-e37e655d4f4a?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />  */}
+            <Card.Img src="https://images.unsplash.com/photo-1515643395264-3f8c19a2dc77?q=80&w=1831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /> 
+        </Card>
             {/* 
                 Display other compononent here that I want on the home page (bootstrap or self made) Like section or content
             */}
